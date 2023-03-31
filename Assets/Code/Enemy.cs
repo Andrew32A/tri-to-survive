c0ade5d2d618cc50f1b3e9d3564f666d3cf4c3fc
Andrew Alsing <andrew32alsing@gmail.com>
2023-03-30 20:29:35 -0700

    enemy now tracks and looks at the player

--- Assets/Code/Enemy.cs
+++ Assets/Code/Enemy.cs
@@ -7,6 +7,8 @@ public class Enemy : MonoBehaviour
     public int health = 100;
     private int currentHealth;
 
+    public Player player;
+
     // TODO: add death explosion here and in Die method
     // public GameObject deathEffect;
 
@@ -18,7 +20,11 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(player != null) {
+            Vector3 direction = player.transform.position - transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
     }
 
     public void TakeDamage(int damage) {
