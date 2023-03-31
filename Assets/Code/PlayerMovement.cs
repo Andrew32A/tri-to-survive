8c78a5c4171454ddde06aafdc1e267f06163090c
Andrew Alsing <andrew32alsing@gmail.com>
2023-03-30 18:03:29 -0700

    added player movement script

---
+++ Assets/Code/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 8.5f;
+    public int rotationOffset = 0;
+
+    private Vector2 movement;
+    private Vector2 mousePos;
+
+    private Camera cam;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        cam = Camera.main;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void FixedUpdate() {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - rotationOffset;
+
+        rb.rotation = angle;
+    }
+}
