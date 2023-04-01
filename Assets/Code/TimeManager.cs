003c49c9e9f0bd781316a36803f62f2978a9f08c
Andrew Alsing <andrew32alsing@gmail.com>
2023-03-31 21:38:40 -0700

    added bg music and lowered pitch during bullet time

--- Assets/Code/TimeManager.cs
+++ Assets/Code/TimeManager.cs
@@ -8,6 +8,10 @@ public class TimeManager : MonoBehaviour
 {
     public float slowdownFactor = 0.05f; // lower the number, the slower it will be
     public float slowdownLength = 4f;
+    
+    public AudioSource audioSource;
+    private float originalPitch;
+
     public Volume postProcessVolume;
     private DepthOfField depthOfField;
     private Vignette vignette;
@@ -18,20 +22,34 @@ public class TimeManager : MonoBehaviour
         postProcessVolume.profile.TryGet(out depthOfField);
         postProcessVolume.profile.TryGet(out vignette);
         postProcessVolume.profile.TryGet(out lensDistortion);
+
+        // store the original pitch of the audio source
+        originalPitch = audioSource.pitch;
     }
 
     void Update() {
         Time.timeScale += (1f / slowdownLength) * Time.unscaledDeltaTime;
-        Time.timeScale = Mathf.Clamp (Time.timeScale, 0f, 1f);
+        Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f);
 
         // disable post processing effect when not in bullet time
         depthOfField.active = (Time.timeScale < 1f);
         vignette.active = (Time.timeScale < 1f);
         lensDistortion.active = (Time.timeScale < 1f);
+
+        // set min and max audio pitch
+        audioSource.pitch = Mathf.Clamp(audioSource.pitch, 0.8f, 1f);
+        
+        // raise the pitch slowly back to normal
+        if (Time.timeScale >= 1f)
+        {
+            audioSource.pitch = Mathf.Lerp(audioSource.pitch, originalPitch, Time.deltaTime);
+        }
     }
 
     public void BulletTime() {
         Time.timeScale = slowdownFactor;
         Time.fixedDeltaTime = Time.timeScale * 0.02f;
+
+        audioSource.pitch *= 0.95f;
     }
 }
