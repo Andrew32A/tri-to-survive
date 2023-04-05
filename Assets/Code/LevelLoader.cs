a8c1bd7a6d1399f2b30477283a1e07fce39fefc6
Andrew Alsing <andrew32alsing@gmail.com>
2023-04-05 00:22:06 -0700

    smoothed out transitions and added level loader to all scenes

--- Assets/Code/LevelLoader.cs
+++ Assets/Code/LevelLoader.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class LevelLoader : MonoBehaviour
 {
     public Animator transition;
     public float transitionTime = 1f;
 
-    public void PlayGame() {
+    public void NextLevel() {
         // load next level
         StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
     }
@@ -19,11 +19,12 @@ public class MainMenu : MonoBehaviour
 
     IEnumerator LoadLevel(int levelIndex) {
         // play transition animation
-        transition.SetTrigger("CrossfadeStart");
+        transition.SetTrigger("Transition");
 
         // wait for transition to finish
         yield return new WaitForSeconds(transitionTime);
 
+        // load level
         SceneManager.LoadScene(levelIndex);
     }
 }
