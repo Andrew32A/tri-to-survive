3f2c6ac87f5955d80a08599be2f410e18bb55898
Andrew Alsing <andrew32alsing@gmail.com>
2023-04-05 21:18:26 -0700

    added shotgun

---
+++ Assets/Code/Shotgun.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shotgun : MonoBehaviour, IWeapon
+{
+    public Transform shotgunFirePoint;
+    public GameObject shotgunBulletPrefab;
+    public Animator shotgunFireSquish;
+
+    public float bulletSpread = 40f;
+    public float pellets = 8;
+    private bool canShoot = true;
+    public float fireRate = 0.5f;
+
+    public void Shoot()
+    {
+        if (canShoot)
+        {
+            for (int i = 0; i < pellets; i++)
+            {
+                float randomSpread = Random.Range(-bulletSpread / 2f, bulletSpread / 2f);
+                Quaternion spreadRotation = Quaternion.Euler(0f, 0f, randomSpread);
+                Instantiate(shotgunBulletPrefab, shotgunFirePoint.position, shotgunFirePoint.rotation * spreadRotation);
+            }
+            shotgunFireSquish.SetTrigger("Fire");
+            canShoot = false;
+            StartCoroutine(ResetShootDelay());
+        }
+    }
+
+    private IEnumerator ResetShootDelay()
+    {
+        yield return new WaitForSeconds(fireRate);
+        canShoot = true;
+    }
+}
--- Assets/Code/Weapon.cs
+++ Assets/Code/Weapon.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// menu of functions that it's expecting the class to have
 public interface IWeapon {
     void Shoot();
 }
@@ -11,6 +12,7 @@ public class Weapon : MonoBehaviour
     public IWeapon currentWeapon;
     public GameObject pistolScript;
     public GameObject smgScript;
+    public GameObject shotgunScript;
 
     void Start()
     {
@@ -34,12 +36,16 @@ public class Weapon : MonoBehaviour
             currentWeapon = EquipSmg();
             Debug.Log("smg equipped");
         }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3)) {
+            currentWeapon = EquipShotgun();
+            Debug.Log("shotgun equipped");
+        }
     }
 
     public void ChangeWeapon(GameObject newWeaponObject) {
         currentWeapon = newWeaponObject.GetComponent<IWeapon>();
     }
-
 
     public IWeapon EquipPistol() {
         return pistolScript.GetComponent<IWeapon>();
@@ -49,4 +55,7 @@ public class Weapon : MonoBehaviour
         return smgScript.GetComponent<IWeapon>();
     }
 
+    public IWeapon EquipShotgun() {
+        return shotgunScript.GetComponent<IWeapon>();
+    }
 }
