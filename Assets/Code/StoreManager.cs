571786fe552c25f5f50dca35d0687b8d45b8d14c
Andrew Alsing <andrew32alsing@gmail.com>
2023-04-08 17:49:42 -0700

    updated display resolution and WebGL settings

--- Assets/Code/StoreManager.cs
+++ Assets/Code/StoreManager.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class StoreManager : MonoBehaviour
 {
     public int playerCurrency = 0;
+
+    public TextMeshProUGUI currencyText;
 
     void Start()
     {
@@ -19,5 +22,7 @@ public class StoreManager : MonoBehaviour
     public void addCurrency() {
         playerCurrency += 1;
         Debug.Log(playerCurrency);
+
+        currencyText.text = playerCurrency.ToString();
     }
 }
